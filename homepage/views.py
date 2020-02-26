831ee33503f928cca9f474b5391339e763aeac0f
Alex <menedgeralex@gmail.com>
2020-02-26 17:09:40 +0300

    Release version 1.0

--- homepage/views.py
+++ homepage/views.py
@@ -1,16 +1,18 @@
 import datetime
 
-from django.contrib.auth.decorators import login_required
-from django.http import HttpResponse, Http404
-from django.views import View
+# from django.contrib.auth.decorators import login_required
+# from django.http import HttpResponse, Http404
+# from django.views import View
 from django.views.generic import TemplateView, ListView
+from users.forms import RegistrationForm
 
 # from questions.models import Question
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 
 
 def index_view(request):
     if request.method == 'GET':
+        form = RegistrationForm()
         if request.user.is_authenticated:
             is_exists = render(request, "index.html", context={"now": datetime.datetime.now()})
             # is_exists = request.user.test_suites.filter(
@@ -23,9 +25,29 @@ def index_view(request):
             "index.html",
             context={
                 "now": datetime.datetime.now(),
-                "is_exists": is_exists
+                "is_exists": is_exists,
+                "form": form
             }
         )
+    elif request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            # login(request, user)
+            # link = user.get_verification_link
+            # user.email_user(
+            #     "Email confirmation",
+            #     f"Please follow the <a href='{link}'>link</a>",
+            #     from_email='menedgeralex@gmail.com'
+            # )
+            # user.verification_email_sent_at = timezone.now()
+            # user.save()
+            return redirect("/login/")
+        else:
+            return render(request, 'index.html', context={
+                "now": datetime.datetime.now(),
+                "form": form
+            })
 
 
 class AboutView(TemplateView):
--- jboard/models.py
+++ jboard/models.py
@@ -5,9 +5,15 @@ from django.db import models
 class Parsers(models.Model):
     link = models.CharField(max_length=200)
     title = models.CharField(max_length=200)
-    description = models.CharField(max_length=300)
+    description = models.TextField()
     position = models.IntegerField()
     update = models.DateTimeField()
+
+    def __unicode__(self):
+        return self.position
+
+    class Meta:
+        ordering = ["position"]
 
 
 # result = Parsers(link='https://vironit.com/', position="1", update='2020-02-24 22:40')
